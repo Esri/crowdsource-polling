0a882ae8e814fa08a1aca83dfe92282a4099f104
MikeTschudi <mtschudi@esri.com>
2015-01-30 07:13:54 -0800

    Removed superfluous include

--- js/widgets/SidebarHeader/SidebarHeader.js
+++ js/widgets/SidebarHeader/SidebarHeader.js
@@ -20,7 +20,6 @@ define([
     "dojo/_base/declare",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
-    "dijit/_WidgetsInTemplateMixin",
     "dojo/text!./templates/SidebarHeader.html",
     "dojo/dom",
     "dojo/_base/lang",
@@ -30,14 +29,13 @@ define([
     declare,
     _WidgetBase,
     _TemplatedMixin,
-    _WidgetsInTemplateMixin,
     template,
     dom,
     lang,
     on,
     SvgHelper
 ) {
-    return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+    return declare([_WidgetBase, _TemplatedMixin], {
         templateString: template,
         signInBtnOnClick: null,
         helpBtnOnClick: null,

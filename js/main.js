6b6ae5a37db038358c9e21553f28c322625e91b9
MikeTschudi <mtschudi@esri.com>
2015-02-17 14:55:48 -0800

    Added controls for linking item list to map extents

--- js/main.js
+++ js/main.js
@@ -78,6 +78,7 @@ define([
         config: {},
         map: null,
         mapData: null,
+        _linkToMapView: true,
         mockCurrentItem: null,  //???
 
         startup: function (config) {
@@ -232,6 +233,13 @@ define([
                     }
                 }));
 
+                topic.subscribe("linkToMapViewSelected", lang.hitch(this, function () {
+                    console.log(">linkToMapViewSelected>", !this._linkToMapView);  //???
+                    this._linkToMapView = !this._linkToMapView;
+                    //this._itemsList.setLinkToMapView(this._linkToMapView);
+                    topic.publish("updateItems");
+                }));
+
                 /**
                  * @param {string} err Error message to display
                  */
@@ -247,6 +255,10 @@ define([
                 topic.subscribe("showPanel", lang.hitch(this, function (name) {
                     console.log(">showPanel>", name);  //???
                     this._sidebarCnt.showPanel(name);
+
+                    if (name === "itemsList") {
+                        topic.publish("updateItems");
+                    }
                 }));
 
                 topic.subscribe("signinUpdate", lang.hitch(this, function () {
@@ -297,14 +309,10 @@ define([
                     this._sidebarCnt.showBusy(false);
                 }));
 
-                /**
-                 * @param {extent} extent Extent limit for the items to search for; use null
-                 * only items within the current map display extents (true) are to be queried
-                 */
-                topic.subscribe("updateItems", lang.hitch(this, function (extent) {
-                    console.log(">updateItems>", extent);  //???
+                topic.subscribe("updateItems", lang.hitch(this, function () {
+                    console.log(">updateItems>");  //???
                     this._sidebarCnt.showBusy(true);
-                    this._mapData.queryItems(extent);
+                    this._mapData.queryItems(this._linkToMapView ? this.map.extent : null);
                 }));
 
                 /**
@@ -313,7 +321,6 @@ define([
                 topic.subscribe("updateVotes", lang.hitch(this, function (item) {
                     console.log(">updateVotes>", item);  //???
                     //this._itemsList.updateVotes(item);
-                    topic.publish("updateItems");
                 }));
 
                 /**
@@ -343,9 +350,15 @@ define([
                     }
                 });
 
-                // Start with items list using map display extents to limit
+                // Support option to reset items list whenever the map is resized
+                on(this.map, "extent-change", lang.hitch(this, function (evt) {
+                    if (this._linkToMapView) {
+                        topic.publish("updateItems");
+                    }
+                }));
+
+                // Start with items list
                 topic.publish("showPanel", "itemsList");
-                topic.publish("updateItems", this.map.extent);
                 topic.publish("signinUpdate");
 
 

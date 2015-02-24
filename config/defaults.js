017f34e27b6ba05aedee1119d1550ff465c797e7
MikeTschudi <mtschudi@esri.com>
2015-02-24 15:31:38 -0800

    Added sample webmaps

--- config/defaults.js
+++ config/defaults.js
@@ -46,6 +46,23 @@ define({
     "twitterCallbackPage": "/oauth-callback.html",
     "socialMediaDisclaimer": "<b>Disclaimer</b><hr>Proin tristique volutpat velit ut mollis. Nulla tincidunt diam nec orci lobortis cursus et ut metus. Duis vel posuere nulla, sit amet sollicitudin tellus. Vivamus molestie vestibulum velit, nec lobortis felis scelerisque a. Ut ac ex odio. Nam finibus elit auctor turpis varius bibendum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut augue ipsum, eleifend at ante ut, tincidunt sollicitudin justo. In vitae magna faucibus, mattis arcu sed, dictum justo. Sed porta sem velit, eu egestas odio pretium at. Quisque condimentum efficitur consequat. Suspendisse eu eros volutpat, ultrices nunc quis, pellentesque turpis. In vitae sodales sapien, eget tincidunt lectus. Proin vitae tempus nisi, quis posuere urna. Mauris vestibulum augue vel nisi lobortis sollicitudin. Maecenas elementum, ante vel auctor pharetra, nisl nunc tempor ante, quis blandit dolor enim at ex.<br><br>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce non neque accumsan, elementum sem vel, blandit odio. Etiam scelerisque iaculis aliquet. Ut varius turpis felis, fringilla placerat purus aliquet vitae. Integer orci elit, iaculis et diam eu, ultricies luctus magna. Donec semper felis et rutrum egestas. Phasellus congue scelerisque diam, sit amet fringilla ipsum aliquam sed. Ut scelerisque, ipsum sit amet suscipit elementum, leo massa eleifend dui, nec pretium ligula urna mollis ipsum. Donec porttitor dui eget porta facilisis. Proin ullamcorper sapien sit amet ultricies fringilla. Nunc ex velit, lobortis at urna in, pellentesque feugiat risus.<br><br>Nam pellentesque enim ex, a pulvinar ante placerat ac. Proin at leo sit amet metus aliquam tincidunt eget eu urna. Nulla ullamcorper facilisis facilisis. Pellentesque non erat orci. Suspendisse odio arcu, porta tempus aliquam ac, elementum in nibh. Etiam suscipit orci at turpis tempor rutrum. Fusce mollis orci in diam iaculis elementum. Vivamus ligula elit, vestibulum nec lectus vel, porta vulputate turpis. Nunc ut libero pellentesque, pellentesque est in, ultrices felis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed gravida vitae mi vel placerat. Ut consectetur consectetur diam, at blandit quam posuere vel.",
 
+
+    //"webmap": "f4271409b18e46d8836a8f52af72583c",  // Crowdsourcing Locations map nopopups
+    //"webmap": "3e738c780fcc43c1b1a5f123dd0d3f6c",  // Crowdsourcing Locations map w/popups
+    //"ideaFields": "source,when_,popularity",
+    //"commentFields": "commenter,when_",
+
+    //"webmap": "e8369b4cfd1d4599a3b07e75109ba589",  // Crowdsourcing Areas map nopopup
+    //"webmap": "c17b229474da4cdcac164bfc145497f4",  // Crowdsourcing Areas map w/popup
+    //"ideaFields": "postedBy,on_,votes",
+    //"commentFields": "who,date",
+
+    //"webmap": "79aaf9c6d60b4d5f91fcc56a4f000e99",  // Crowdsourcing Routes map nopopup
+    //"webmap": "1b7096212a034006a40765967ecff0b9",  // Crowdsourcing Routes map w/popup
+    //"ideaFields": "by_,posted,likes",
+    //"commentFields": "by_,posted",
+
+
     //Other template-specific properties
     "helpIcon": "[{'shape':{'type':'path','path':'M 9.6 0C 4.4 0 0.1 4.3 0.1 9.5 0.1 14.7 4.4 19 9.6 19 14.8 19 19.1 14.7 19.1 9.5 19.1 4.3 14.8 0 9.6 0zM 11 15.5c 0 0.2-0.1 0.3-0.3 0.3H 8.6c-0.2 0-0.3-0.1-0.3-0.3v-2.1c 0-0.2 0.1-0.3 0.3-0.3h 2.1c 0.2 0 0.3 0.1 0.3 0.3v 2.1zM 13 8.4C 12.8 8.7 12.4 9 11.9 9.5l-0.5 0.4c-0.2 0.2-0.4 0.4-0.4 0.6 0 0.1-0.1 0.3-0.1 0.8 0 0.2-0.1 0.3-0.3 0.3H 8.7c-0.1 0-0.2 0-0.2-0.1C 8.4 11.4 8.4 11.4 8.4 11.3 8.4 10.4 8.5 9.9 8.7 9.5 8.8 9.1 9.2 8.7 9.8 8.3L 10.3 7.9C 10.4 7.8 10.6 7.7 10.6 7.5 10.8 7.3 10.8 7.1 10.8 6.8 10.8 6.5 10.7 6.2 10.5 6 10.4 5.8 10 5.7 9.6 5.7 9.2 5.7 8.9 5.8 8.7 6.1 8.5 6.4 8.4 6.7 8.4 7.1 8.4 7.3 8.3 7.4 8.1 7.4H 6C 5.9 7.4 5.8 7.4 5.8 7.3 5.7 7.2 5.7 7.2 5.7 7.1 5.8 5.6 6.3 4.6 7.3 3.9 7.9 3.5 8.7 3.3 9.6 3.3c 1.1 0 2.1 0.3 2.9 0.8 0.8 0.6 1.2 1.4 1.2 2.6-0.1 0.6-0.3 1.2-0.7 1.7z'},'fill':{'r':0,'g':122,'b':194,'a':1}}]",
     "likeIcon": "[{'shape': {'type': 'path', 'path': 'm 11.017346,1.9360981 c -0.3,-0.7 -0.800001,-1.30000004 -1.6000008,-1.6 -0.7,-0.3 -1.4,-0.4 -2.1,-0.2 -0.7,0.2 -1.3,0.69999996 -1.7,1.3 -0.4,-0.60000004 -1,-1.1 -1.7,-1.3 -0.7,-0.2 -1.4,-0.2 -2.1,0.2 -0.8,0.39999996 -1.30000006,0.9 -1.60000006,1.6 -0.3,0.7 -0.3,1.5 0.1,2.3 0.70000006,1.7 5.20000006,5.7000001 5.20000006,5.7999999 0,-0.1 4.4999998,-4.0999999 5.2000008,-5.7999999 0.6,-0.8 0.6,-1.6 0.3,-2.3 z'},'fill':{'r':0,'g':0,'b':0,'a':.35}}]",

e8977ebe526197cfc42df634f24454dbff7a69ea
Allison Muise <amuise@esri.com>
2015-08-25 12:04:42 -0700

    fixed missing comma

--- resources/configurationPanel.js
+++ resources/configurationPanel.js
@@ -1,6 +1,6 @@
 {
     "values": {
-        "webmap": "56197689ee7e4a8aa9f0d8da09ffe721"
+        "webmap": "56197689ee7e4a8aa9f0d8da09ffe721",
         "title": "Crowdsource Polling",
         "displayText": "<p><b>Welcome to the crowdsource polling application</b></p><p>Use Crowdsource Polling to provide information and collect feedback on plans and issues around your community.</p><p>Search for a location or click an item in the list to get started.</p><hr/>",
         "color": "#206bdb",

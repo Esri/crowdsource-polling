9d6ce8085f6cc7e0a65ed41b90c2201b8d6f4102
MikeTschudi <mtschudi@esri.com>
2016-06-05 09:28:00 -0700

    Updated language files

--- js/nls/hr/resources.js
+++ js/nls/hr/resources.js
@@ -14,57 +14,55 @@
  | See the License for the specific language governing permissions and
  | limitations under the License.
  */
-define(
-     ({
-        map: {  // Map, feature layer, and comments table loading and checking
-            error: "Nije moguće stvoriti kartu",
-            layerLoad: "Nije moguće učitati kartu do kraja",
-            missingItemsFeatureLayer: "Konfigurirajte aplikaciju za upotrebu sloja geoobjekata"
-        },
-        sidebar_header: {  // Top panel of right-side display; contains social media sign-in, help button, title
-            menuButtonTooltip: "Prikaz izbornika",
-            signInButton: "Prijava",
-            signInButtonTooltip: "Prijava",
-            signOutButton: "Odjava",
-            signOutButtonTooltip: "Odjava",
-            helpButtonLabel: "Pomoć",
-            helpButtonTooltip: "Saznaj više",
-            gotoListViewLabel: "Prikaz u popisu",
-            gotoListViewTooltip: "Idi na prikaz u popisu", // Go to List view tooltip text
-            gotoMapViewLabel: "Prikaz karte",
-            gotoMapViewTooltip: "Idi na prikaz karte" // Tooltip for map-it icon in list header
-        },
-        popup_Close: {  // Close button for help and social-media-sign-in popup box
-            closeButtonTooltip: "Zatvori"
-        },
-        social_media: {  // Social media sign-in/out
-            signIntoFacebookTooltip: "Prijava preko Facebooka",
-            signIntoGooglePlusTooltip: "Prijava preko Google+",
-            signIntoTwitterTooltip: "Prijava preko Twittera",
-            signOutOfFacebookTooltip: "Odjava s Facebooka",
-            signOutOfGooglePlusTooltip: "Odjava s Google+",
-            signOutOfTwitterTooltip: "Odjava s Twittera"
-        },
-        dynamic_form: {  // General-purpose form; used to receive comment entry
-            optionalFormItemFlag: " (neobavezno)",
-            requiredFormItemFlag: " (obavezno)",
-            unsettableRequiredField: "Obavezno polje nije ni započeto ni u obrascu",
-            countOfRemainingCharactersTooltip: "Preostalo znakova",
-            cancelButtonLabel: "Odustani",
-            submitButtonLabel: "Pošalji"
-        },
-        item_details: {  // Detailed information about an item and a list of its comments
-            likeButtonTooltip: "Glasaj za ovu stavku",
-            commentButtonTooltip: "Dodaj komentar",
-            gotoMapViewTooltip: "Idi na prikaz karte",
-            galleryButtonTooltip: "Vidi datoteke u privitku",
-            commentsListHeading: "Komentari",
-            noCommentsPlaceholder: "Nema komentara"
-        },
-        item_list: {  // List of feature layer items
-            linkToMapViewOptionLabel: "Filtriraj popis prema karti",
-            linkToMapViewOptionTooltip: "Popis geoobjekata koji su vidljivi na trenutačnoj karti",
-            likesForThisItemTooltip: "Glasovi za ovu stavku"
-        }
-    })
-);
+define({
+  "map": {
+    "error": "Nije moguće stvoriti kartu",
+    "layerLoad": "Nije moguće učitati kartu do kraja",
+    "missingItemsFeatureLayer": "Konfigurirajte aplikaciju za upotrebu sloja geoobjekata"
+  },
+  "sidebar_header": {
+    "menuButtonTooltip": "Prikaz izbornika",
+    "signInButton": "Prijava",
+    "signInButtonTooltip": "Prijava",
+    "signOutButton": "Odjava",
+    "signOutButtonTooltip": "Odjava",
+    "helpButtonLabel": "Pomoć",
+    "helpButtonTooltip": "Saznaj više",
+    "gotoListViewLabel": "Prikaz u popisu",
+    "gotoListViewTooltip": "Idi na prikaz u popisu",
+    "gotoMapViewLabel": "Prikaz karte",
+    "gotoMapViewTooltip": "Idi na prikaz karte"
+  },
+  "popup_Close": {
+    "closeButtonTooltip": "Zatvori"
+  },
+  "social_media": {
+    "signIntoFacebookTooltip": "Prijava preko Facebooka",
+    "signIntoGooglePlusTooltip": "Prijava preko Google+",
+    "signIntoTwitterTooltip": "Prijava preko Twittera",
+    "signOutOfFacebookTooltip": "Odjava s Facebooka",
+    "signOutOfGooglePlusTooltip": "Odjava s Google+",
+    "signOutOfTwitterTooltip": "Odjava s Twittera"
+  },
+  "dynamic_form": {
+    "optionalFormItemFlag": " (neobavezno)",
+    "requiredFormItemFlag": " (obavezno)",
+    "unsettableRequiredField": "Obavezno polje nije ni započeto ni u obrascu",
+    "countOfRemainingCharactersTooltip": "Preostalo znakova",
+    "cancelButtonLabel": "Odustani",
+    "submitButtonLabel": "Pošalji"
+  },
+  "item_details": {
+    "likeButtonTooltip": "Glasaj za ovu stavku",
+    "commentButtonTooltip": "Dodaj komentar",
+    "gotoMapViewTooltip": "Idi na prikaz karte",
+    "galleryButtonTooltip": "Vidi datoteke u privitku",
+    "commentsListHeading": "Komentari",
+    "noCommentsPlaceholder": "Nema komentara"
+  },
+  "item_list": {
+    "linkToMapViewOptionLabel": "Filtriraj popis prema karti",
+    "linkToMapViewOptionTooltip": "Popis geoobjekata koji su vidljivi na trenutačnoj karti",
+    "likesForThisItemTooltip": "Glasovi za ovu stavku"
+  }
+});
--- js/nls/ru/resources.js
+++ js/nls/ru/resources.js
@@ -14,57 +14,55 @@
  | See the License for the specific language governing permissions and
  | limitations under the License.
  */
-define(
-     ({
-        map: {  // Map, feature layer, and comments table loading and checking
-            error: "Не удалось создать карту",
-            layerLoad: "Не удалось полностью загрузить карту",
-            missingItemsFeatureLayer: "Настройте приложение для использования векторного слоя."
-        },
-        sidebar_header: {  // Top panel of right-side display; contains social media sign-in, help button, title
-            menuButtonTooltip: "Показать меню",
-            signInButton: "Вход",
-            signInButtonTooltip: "Вход",
-            signOutButton: "Выход",
-            signOutButtonTooltip: "Выход",
-            helpButtonLabel: "Справка",
-            helpButtonTooltip: "Более подробно",
-            gotoListViewLabel: "Вид списка",
-            gotoListViewTooltip: "Перейти в вид списка", // Go to List view tooltip text
-            gotoMapViewLabel: "Вид карты",
-            gotoMapViewTooltip: "Перейти к виду карты" // Tooltip for map-it icon in list header
-        },
-        popup_Close: {  // Close button for help and social-media-sign-in popup box
-            closeButtonTooltip: "Закрыть"
-        },
-        social_media: {  // Social media sign-in/out
-            signIntoFacebookTooltip: "Войти через Facebook",
-            signIntoGooglePlusTooltip: "Войти через Google+",
-            signIntoTwitterTooltip: "Войти через Twitter",
-            signOutOfFacebookTooltip: "Выйти из Facebook",
-            signOutOfGooglePlusTooltip: "Выйти из Google+",
-            signOutOfTwitterTooltip: "Выйти из Twitter"
-        },
-        dynamic_form: {  // General-purpose form; used to receive comment entry
-            optionalFormItemFlag: " (дополнительно)",
-            requiredFormItemFlag: " (необходимо)",
-            unsettableRequiredField: "Требуемое поле неактивно либо отсутствует в форме",
-            countOfRemainingCharactersTooltip: "Символов осталось",
-            cancelButtonLabel: "Отмена",
-            submitButtonLabel: "Подтвердить"
-        },
-        item_details: {  // Detailed information about an item and a list of its comments
-            likeButtonTooltip: "Голосовать за этот элемент",
-            commentButtonTooltip: "Добавить комментарий",
-            gotoMapViewTooltip: "Перейти к виду карты",
-            galleryButtonTooltip: "Просмотр вложенных файлов",
-            commentsListHeading: "Комментарии",
-            noCommentsPlaceholder: "Комментарии отсутствуют"
-        },
-        item_list: {  // List of feature layer items
-            linkToMapViewOptionLabel: "Фильтр списка по картам",
-            linkToMapViewOptionTooltip: "Список объектов, видимых в текущей карте",
-            likesForThisItemTooltip: "Голоса, поданные за этот элемент"
-        }
-    })
-);
+define({
+  "map": {
+    "error": "Не удалось создать карту",
+    "layerLoad": "Не удалось полностью загрузить карту",
+    "missingItemsFeatureLayer": "Настройте приложение для использования векторного слоя."
+  },
+  "sidebar_header": {
+    "menuButtonTooltip": "Показать меню",
+    "signInButton": "Вход",
+    "signInButtonTooltip": "Вход",
+    "signOutButton": "Выход",
+    "signOutButtonTooltip": "Выход",
+    "helpButtonLabel": "Справка",
+    "helpButtonTooltip": "Более подробно",
+    "gotoListViewLabel": "Вид списка",
+    "gotoListViewTooltip": "Перейти в вид списка",
+    "gotoMapViewLabel": "Вид карты",
+    "gotoMapViewTooltip": "Перейти к виду карты"
+  },
+  "popup_Close": {
+    "closeButtonTooltip": "Закрыть"
+  },
+  "social_media": {
+    "signIntoFacebookTooltip": "Войти через Facebook",
+    "signIntoGooglePlusTooltip": "Войти через Google+",
+    "signIntoTwitterTooltip": "Войти через Twitter",
+    "signOutOfFacebookTooltip": "Выйти из Facebook",
+    "signOutOfGooglePlusTooltip": "Выйти из Google+",
+    "signOutOfTwitterTooltip": "Выйти из Twitter"
+  },
+  "dynamic_form": {
+    "optionalFormItemFlag": " (дополнительно)",
+    "requiredFormItemFlag": " (необходимо)",
+    "unsettableRequiredField": "Требуемое поле неактивно либо отсутствует в форме",
+    "countOfRemainingCharactersTooltip": "Символов осталось",
+    "cancelButtonLabel": "Отмена",
+    "submitButtonLabel": "Подтвердить"
+  },
+  "item_details": {
+    "likeButtonTooltip": "Голосовать за этот элемент",
+    "commentButtonTooltip": "Добавить комментарий",
+    "gotoMapViewTooltip": "Перейти к виду карты",
+    "galleryButtonTooltip": "Просмотр вложенных файлов",
+    "commentsListHeading": "Комментарии",
+    "noCommentsPlaceholder": "Комментарии отсутствуют"
+  },
+  "item_list": {
+    "linkToMapViewOptionLabel": "Фильтр списка по картам",
+    "linkToMapViewOptionTooltip": "Список объектов, видимых в текущей карте",
+    "likesForThisItemTooltip": "Голоса, поданные за этот элемент"
+  }
+});

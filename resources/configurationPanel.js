f6c1b81704fe1655d2194dca2dd4f0a1eca607b8
MikeTschudi <mtschudi@esri.com>
2016-10-25 16:23:43 -0700

    Fixed type spec in configurationPanel

--- resources/configurationPanel.js
+++ resources/configurationPanel.js
@@ -73,14 +73,14 @@
                 "geometryTypes": ["esriGeometryPoint", "esriGeometryLine", "esriGeometryPolyline", "esriGeometryPolygon"]
             },
             "fields": [{
-                "supportedTypes": ["esriFieldTypeSmallInteger", "esriFieldTypeInteger"],
+                "supportedTypes": ["esriFieldTypeSmallInteger", "esriFieldTypeInteger", "esriFieldTypeSingle",
+                    "esriFieldTypeDouble", "esriFieldTypeString", "esriFieldTypeDate"],
                 "multipleSelection": false,
                 "fieldName": "sortField",
                 "label": "Field to use to sort items in app (optional)",
                 "tooltip": "Field in the selected layer for sorting items listed in app. If omitted, items are not sorted."
             }, {
-                "supportedTypes": ["esriFieldTypeSmallInteger", "esriFieldTypeInteger", "esriFieldTypeSingle",
-                    "esriFieldTypeDouble", "esriFieldTypeString", "esriFieldTypeDate"],
+                "supportedTypes": ["esriFieldTypeSmallInteger", "esriFieldTypeInteger"],
                 "multipleSelection": false,
                 "fieldName": "itemVotesField",
                 "label": "Field to store the vote tally for each report (optional)",

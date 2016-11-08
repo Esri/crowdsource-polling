69856d9dc9a55ccc477505d1dc6db06982249f3e
MikeTschudi <mtschudi@esri.com>
2016-11-08 09:18:08 -0800

    Added Grunt files for linting

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+
+  // Project tasks configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+        jshintrc: true,
+        ignores: ['js/lib/modernizr*.js']
+      },
+      src: 'js/**/*.js'
+    }
+  });
+
+  // Load grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+};

57de69f19208182f2d771f7e08889696de4feba0
Mike Tschudi <mtschudi@esri.com>
2017-10-25 15:43:37 -0700

    Add Slovenian language placeholder

---
+++ js/nls/sl/resources.js
@@ -0,0 +1,74 @@
+﻿/*
+ | Copyright 2014 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
+define({
+  "map": {
+    "error": "Š_Unable to create map_______č",
+    "layerLoad": "Š_Unable to fully load map________č",
+    "missingItemsFeatureLayer": "Š_Please configure the application to use a feature layer__________________č"
+  },
+  "sidebar_header": {
+    "menuButtonTooltip": "Š_Show menu____č",
+    "signInButton": "Š_Sign In___č",
+    "signInButtonTooltip": "Š_Sign in___č",
+    "signOutButton": "Š_Sign Out___č",
+    "signOutButtonTooltip": "Š_Sign out___č",
+    "helpButtonLabel": "Š_Help__č",
+    "helpButtonTooltip": "Š_Learn more____č",
+    "gotoListViewLabel": "Š_List View____č",
+    "gotoListViewTooltip": "Š_Go to list view______č",
+    "gotoMapViewLabel": "Š_Map View___č",
+    "gotoMapViewTooltip": "Š_Go to map view_____č"
+  },
+  "popup_Close": {
+    "closeButtonTooltip": "Š_Close___č"
+  },
+  "social_media": {
+    "signInHeaderTitle": "Š_Social Media Sign In_______č",
+    "signIntoFacebookTooltip": "Š_Sign in with Facebook_______č",
+    "signIntoGooglePlusTooltip": "Š_Sign in with Google+_______č",
+    "signIntoTwitterTooltip": "Š_Sign in with Twitter_______č",
+    "signOutOfFacebookTooltip": "Š_Sign out of Facebook_______č",
+    "signOutOfGooglePlusTooltip": "Š_Sign out of Google+_______č",
+    "signOutOfTwitterTooltip": "Š_Sign out of Twitter_______č"
+  },
+  "dynamic_form": {
+    "optionalFormItemFlag": " Š_(optional)____č",
+    "requiredFormItemFlag": " Š_(required)____č",
+    "unsettableRequiredField": "Š_A required field is neither initialized nor in the form__________________č",
+    "countOfRemainingCharactersTooltip": "Š_Characters remaining_______č",
+    "attachmentsHeading": "Š_Attachments____č",
+    "addAttachmentTooltip": "Š_Add attachment_____č",
+    "removeAttachmentTooltip": "Š_Remove attachment______č",
+    "cancelButtonLabel": "Š_Cancel___č",
+    "submitButtonLabel": "Š_Submit___č"
+  },
+  "item_details": {
+    "likeButtonTooltip": "Š_Vote for this item______č",
+    "likeButtonInverseTooltip": "Š_Vote counted_____č",
+    "commentButtonTooltip": "Š_Add a comment_____č",
+    "gotoMapViewTooltip": "Š_Go to map view_____č",
+    "galleryButtonTooltip": "Š_See attached files______č",
+    "commentsListHeading": "Š_Comments___č",
+    "noCommentsPlaceholder": "Š_No comments____č",
+    "numberOfAttachmentsUploaded": "Š_${0} attachment(s) uploaded_________č",
+    "numberOfAttachmentsUploadedAndFailed": "Š_${0} attachment(s) uploaded<br>${1} attachment(s) could not be uploaded______________________č"
+  },
+  "item_list": {
+    "linkToMapViewOptionLabel": "Š_Filter list by map______č",
+    "linkToMapViewOptionTooltip": "Š_List features that are visible in the current map________________č",
+    "likesForThisItemTooltip": "Š_Votes for this item_______č"
+  }
+});

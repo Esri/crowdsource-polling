9f39d4ef6f82908295a16025ff77a0c5f606179f
Mike Tschudi <mtschudi@esri.com>
2017-11-13 21:29:25 -0800

    Updated Slovenian

--- js/nls/sl/resources.js
+++ js/nls/sl/resources.js
@@ -15,60 +15,60 @@
  */
 define({
   "map": {
-    "error": "Š_Unable to create map_______č",
-    "layerLoad": "Š_Unable to fully load map________č",
-    "missingItemsFeatureLayer": "Š_Please configure the application to use a feature layer__________________č"
+    "error": "Karte ni mogoče ustvariti",
+    "layerLoad": "Karte ni mogoče v celoti naložiti",
+    "missingItemsFeatureLayer": "Konfigurirajte aplikacijo za uporabo geoobjektnega sloja"
   },
   "sidebar_header": {
-    "menuButtonTooltip": "Š_Show menu____č",
-    "signInButton": "Š_Sign In___č",
-    "signInButtonTooltip": "Š_Sign in___č",
-    "signOutButton": "Š_Sign Out___č",
-    "signOutButtonTooltip": "Š_Sign out___č",
-    "helpButtonLabel": "Š_Help__č",
-    "helpButtonTooltip": "Š_Learn more____č",
-    "gotoListViewLabel": "Š_List View____č",
-    "gotoListViewTooltip": "Š_Go to list view______č",
-    "gotoMapViewLabel": "Š_Map View___č",
-    "gotoMapViewTooltip": "Š_Go to map view_____č"
+    "menuButtonTooltip": "Prikaži meni",
+    "signInButton": "Prijava",
+    "signInButtonTooltip": "Prijava",
+    "signOutButton": "Odjava",
+    "signOutButtonTooltip": "Odjava",
+    "helpButtonLabel": "Pomoč",
+    "helpButtonTooltip": "Izvedite več",
+    "gotoListViewLabel": "Pogled seznama",
+    "gotoListViewTooltip": "Pojdi na pogled seznama",
+    "gotoMapViewLabel": "Pogled karte",
+    "gotoMapViewTooltip": "Pojdi na pogled karte"
   },
   "popup_Close": {
-    "closeButtonTooltip": "Š_Close___č"
+    "closeButtonTooltip": "Zapri"
   },
   "social_media": {
-    "signInHeaderTitle": "Š_Social Media Sign In_______č",
-    "signIntoFacebookTooltip": "Š_Sign in with Facebook_______č",
-    "signIntoGooglePlusTooltip": "Š_Sign in with Google+_______č",
-    "signIntoTwitterTooltip": "Š_Sign in with Twitter_______č",
-    "signOutOfFacebookTooltip": "Š_Sign out of Facebook_______č",
-    "signOutOfGooglePlusTooltip": "Š_Sign out of Google+_______č",
-    "signOutOfTwitterTooltip": "Š_Sign out of Twitter_______č"
+    "signInHeaderTitle": "Prijave družbenih omrežij",
+    "signIntoFacebookTooltip": "Prijavite se z računom Facebook",
+    "signIntoGooglePlusTooltip": "Prijavite se z računom Google+",
+    "signIntoTwitterTooltip": "Prijavite se z računom Twitter",
+    "signOutOfFacebookTooltip": "Odjavite se iz Facebooka",
+    "signOutOfGooglePlusTooltip": "Odjavite se iz Google+",
+    "signOutOfTwitterTooltip": "Odjavite se iz Twitterja"
   },
   "dynamic_form": {
-    "optionalFormItemFlag": " Š_(optional)____č",
-    "requiredFormItemFlag": " Š_(required)____č",
-    "unsettableRequiredField": "Š_A required field is neither initialized nor in the form__________________č",
-    "countOfRemainingCharactersTooltip": "Š_Characters remaining_______č",
-    "attachmentsHeading": "Š_Attachments____č",
-    "addAttachmentTooltip": "Š_Add attachment_____č",
-    "removeAttachmentTooltip": "Š_Remove attachment______č",
-    "cancelButtonLabel": "Š_Cancel___č",
-    "submitButtonLabel": "Š_Submit___č"
+    "optionalFormItemFlag": " (izbirno)",
+    "requiredFormItemFlag": " (zahtevano)",
+    "unsettableRequiredField": "Zahtevano polje ni niti inicializirano niti v obrazcu",
+    "countOfRemainingCharactersTooltip": "Število preostalih znakov",
+    "attachmentsHeading": "Priloge",
+    "addAttachmentTooltip": "Dodaj prilogo",
+    "removeAttachmentTooltip": "Odstrani prilogo",
+    "cancelButtonLabel": "Prekliči",
+    "submitButtonLabel": "Pošlji"
   },
   "item_details": {
-    "likeButtonTooltip": "Š_Vote for this item______č",
-    "likeButtonInverseTooltip": "Š_Vote counted_____č",
-    "commentButtonTooltip": "Š_Add a comment_____č",
-    "gotoMapViewTooltip": "Š_Go to map view_____č",
-    "galleryButtonTooltip": "Š_See attached files______č",
-    "commentsListHeading": "Š_Comments___č",
-    "noCommentsPlaceholder": "Š_No comments____č",
-    "numberOfAttachmentsUploaded": "Š_${0} attachment(s) uploaded_________č",
-    "numberOfAttachmentsUploadedAndFailed": "Š_${0} attachment(s) uploaded<br>${1} attachment(s) could not be uploaded______________________č"
+    "likeButtonTooltip": "Glasuj za ta element",
+    "likeButtonInverseTooltip": "Sešteti glasovi",
+    "commentButtonTooltip": "Dodaj komentar",
+    "gotoMapViewTooltip": "Pojdi na pogled karte",
+    "galleryButtonTooltip": "Glej priložene datoteke",
+    "commentsListHeading": "Komentarji",
+    "noCommentsPlaceholder": "Ni komentarjev",
+    "numberOfAttachmentsUploaded": "Naloženih je ${0} prilog",
+    "numberOfAttachmentsUploadedAndFailed": "${0} prilog naloženih <br>${1} prilog ni bilo mogoče naložiti"
   },
   "item_list": {
-    "linkToMapViewOptionLabel": "Š_Filter list by map______č",
-    "linkToMapViewOptionTooltip": "Š_List features that are visible in the current map________________č",
-    "likesForThisItemTooltip": "Š_Votes for this item_______č"
+    "linkToMapViewOptionLabel": "Filtriraj seznam po karti",
+    "linkToMapViewOptionTooltip": "Seznam geoobjektov, ki so vidni na trenutni karti",
+    "likesForThisItemTooltip": "Glasovi za ta element"
   }
 });

9213acc07e257dbe1776f37814df922671f76874
Mike Tschudi <mtschudi@esri.com>
2018-05-09 11:31:17 -0500

    Updated languages

--- js/nls/ca/resources.js
+++ js/nls/ca/resources.js
@@ -14,100 +14,61 @@
  | limitations under the License.
  */
 define({
-    root: ({
-        map: {  // Map, feature layer, and comments table loading and checking
-            error: "No es pot crear el mapa",
-            layerLoad: "No es pot carregar el mapa completament",
-            missingItemsFeatureLayer: "Configureu l\'aplicació perquè utilitzi una capa d\'entitats"
-        },
-        sidebar_header: {  // Top panel of right-side display; contains social media sign-in, help button, title
-            menuButtonTooltip: "Mostra el menú",
-            signInButton: "Inicia la sessió",
-            signInButtonTooltip: "Inicia la sessió",
-            signOutButton: "Tanca la sessió",
-            signOutButtonTooltip: "Tanca la sessió",
-            helpButtonLabel: "Ajuda",
-            helpButtonTooltip: "Més informació",
-            gotoListViewLabel: "Visualització de llista",
-            gotoListViewTooltip: "Ves a la visualització de llista", // Go to List view tooltip text
-            gotoMapViewLabel: "Visualització del mapa",
-            gotoMapViewTooltip: "Ves a la visualització del mapa" // Tooltip for map-it icon in list header
-        },
-        popup_Close: {  // Close button for help and social-media-sign-in popup box
-            closeButtonTooltip: "Tanca"
-        },
-        social_media: {  // Social media sign-in/out
-            signInHeaderTitle : "Inici de sessió a les xarxes socials",
-            signIntoFacebookTooltip: "Inicia la sessió amb el Facebook",
-            signIntoGooglePlusTooltip: "Inicia la sessió amb el Google+",
-            signIntoTwitterTooltip: "Inicia la sessió amb el Twitter",
-            signOutOfFacebookTooltip: "Tanca la sessió del Facebook",
-            signOutOfGooglePlusTooltip: "Tanca la sessió del Google+",
-            signOutOfTwitterTooltip: "Tanca la sessió del Twitter"
-        },
-        dynamic_form: {  // General-purpose form; used to receive comment entry
-            optionalFormItemFlag: " (opcional)",
-            requiredFormItemFlag: " (obligatori)",
-            unsettableRequiredField: "Un camp obligatori no està inicialitzat ni tampoc és al formulari",
-            countOfRemainingCharactersTooltip: "Caràcters restants",
-            attachmentsHeading: "Fitxers adjunts",
-            addAttachmentTooltip: "Afegeix un fitxer adjunt",
-            removeAttachmentTooltip: "Elimina el fitxer adjunt",
-            cancelButtonLabel: "Cancel·la",
-            submitButtonLabel: "Envia"
-        },
-        item_details: {  // Detailed information about an item and a list of its comments
-            likeButtonTooltip: "Vota aquest element",
-            likeButtonInverseTooltip: "S\'ha comptabilitzat el vot",
-            commentButtonTooltip: "Afegeix un comentari",
-            gotoMapViewTooltip: "Ves a la visualització del mapa",
-            galleryButtonTooltip: "Visualitza els fitxers adjunts",
-            commentsListHeading: "Comentaris",
-            noCommentsPlaceholder: "No hi ha cap comentari",
-            numberOfAttachmentsUploaded: "${0} fitxers adjunts pujats",
-            numberOfAttachmentsUploadedAndFailed: "${0} fitxers adjunts pujats<br>${1} fitxers adjunts no s\'han pogut pujar"
-        },
-        item_list: {  // List of feature layer items
-            linkToMapViewOptionLabel: "Filtra la llista per mapa",
-            linkToMapViewOptionTooltip: "Enumera les entitats visibles al mapa actual",
-            likesForThisItemTooltip: "Vots per a aquest element"
-        }
-    }),
-    "ar": 1,
-    "bs": 1,
-    "ca": 0,
-    "cs": 1,
-    "da": 1,
-    "de": 1,
-    "el": 1,
-    "es": 1,
-    "et": 1,
-    "fi": 1,
-    "fr": 1,
-    "he": 1,
-    "hi": 1,
-    "hr": 1,
-    "hu": 0,
-    "id": 1,
-    "it": 1,
-    "ja": 1,
-    "ko": 1,
-    "lt": 1,
-    "lv": 1,
-    "nb": 1,
-    "nl": 1,
-    "pl": 1,
-    "pt-br": 1,
-    "pt-pt": 1,
-    "ro": 1,
-    "ru": 1,
-    "sl": 1,
-    "sr": 1,
-    "sv": 1,
-    "th": 1,
-    "tr": 1,
-    "vi": 1,
-    "zh-cn": 1,
-    "zh-hk": 1,
-    "zh-tw": 1
+  "map": {
+    "error": "No es pot crear el mapa",
+    "layerLoad": "No es pot carregar el mapa completament",
+    "missingItemsFeatureLayer": "Configureu l'aplicació perquè utilitzi una capa d'entitats"
+  },
+  "sidebar_header": {
+    "menuButtonTooltip": "Mostra el menú",
+    "signInButton": "Inicia la sessió",
+    "signInButtonTooltip": "Inicia la sessió",
+    "signOutButton": "Tanca la sessió",
+    "signOutButtonTooltip": "Tanca la sessió",
+    "helpButtonLabel": "Ajuda",
+    "helpButtonTooltip": "Més informació",
+    "gotoListViewLabel": "Visualització de llista",
+    "gotoListViewTooltip": "Ves a la visualització de llista",
+    "gotoMapViewLabel": "Visualització del mapa",
+    "gotoMapViewTooltip": "Ves a la visualització del mapa"
+  },
+  "popup_Close": {
+    "closeButtonTooltip": "Tanca"
+  },
+  "social_media": {
+    "signInHeaderTitle": "Inici de sessió a les xarxes socials",
+    "signIntoFacebookTooltip": "Inicia la sessió amb el Facebook",
+    "signIntoGooglePlusTooltip": "Inicia la sessió amb el Google+",
+    "signIntoTwitterTooltip": "Inicia la sessió amb el Twitter",
+    "signOutOfFacebookTooltip": "Tanca la sessió del Facebook",
+    "signOutOfGooglePlusTooltip": "Tanca la sessió del Google+",
+    "signOutOfTwitterTooltip": "Tanca la sessió del Twitter"
+  },
+  "dynamic_form": {
+    "optionalFormItemFlag": " (opcional)",
+    "requiredFormItemFlag": " (obligatori)",
+    "unsettableRequiredField": "Un camp obligatori no està inicialitzat ni tampoc és al formulari",
+    "countOfRemainingCharactersTooltip": "Caràcters restants",
+    "attachmentsHeading": "Fitxers adjunts",
+    "addAttachmentTooltip": "Afegeix un fitxer adjunt",
+    "removeAttachmentTooltip": "Elimina el fitxer adjunt",
+    "cancelButtonLabel": "Cancel·la",
+    "submitButtonLabel": "Envia"
+  },
+  "item_details": {
+    "likeButtonTooltip": "Vota aquest element",
+    "likeButtonInverseTooltip": "S'ha comptabilitzat el vot",
+    "commentButtonTooltip": "Afegeix un comentari",
+    "gotoMapViewTooltip": "Ves a la visualització del mapa",
+    "galleryButtonTooltip": "Visualitza els fitxers adjunts",
+    "commentsListHeading": "Comentaris",
+    "noCommentsPlaceholder": "No hi ha cap comentari",
+    "numberOfAttachmentsUploaded": "${0} fitxers adjunts pujats",
+    "numberOfAttachmentsUploadedAndFailed": "${0} fitxers adjunts pujats<br>${1} fitxers adjunts no s'han pogut pujar"
+  },
+  "item_list": {
+    "linkToMapViewOptionLabel": "Filtra la llista per mapa",
+    "linkToMapViewOptionTooltip": "Enumera les entitats visibles al mapa actual",
+    "likesForThisItemTooltip": "Vots per a aquest element"
+  }
 });
--- js/nls/hu/resources.js
+++ js/nls/hu/resources.js
@@ -14,100 +14,61 @@
  | limitations under the License.
  */
 define({
-    root: ({
-        map: {  // Map, feature layer, and comments table loading and checking
-            error: "Nem sikerült létrehozni a térképet",
-            layerLoad: "Nem sikerült teljesen betölteni a térképet",
-            missingItemsFeatureLayer: "Konfigurálja az alkalmazást a vektoros réteg használatára"
-        },
-        sidebar_header: {  // Top panel of right-side display; contains social media sign-in, help button, title
-            menuButtonTooltip: "Menü megjelenítése",
-            signInButton: "Bejelentkezés",
-            signInButtonTooltip: "Bejelentkezés",
-            signOutButton: "Kijelentkezés",
-            signOutButtonTooltip: "Kijelentkezés",
-            helpButtonLabel: "Súgó",
-            helpButtonTooltip: "További információk",
-            gotoListViewLabel: "Listanézet",
-            gotoListViewTooltip: "Váltás listanézetre", // Go to List view tooltip text
-            gotoMapViewLabel: "Térképnézet",
-            gotoMapViewTooltip: "Váltás térképnézetre" // Tooltip for map-it icon in list header
-        },
-        popup_Close: {  // Close button for help and social-media-sign-in popup box
-            closeButtonTooltip: "Bezárás"
-        },
-        social_media: {  // Social media sign-in/out
-            signInHeaderTitle : "Bejelentkezés közösségi médián keresztül",
-            signIntoFacebookTooltip: "Bejelentkezés Facebook-azonosítóval",
-            signIntoGooglePlusTooltip: "Bejelentkezés Google+-azonosítóval",
-            signIntoTwitterTooltip: "Bejelentkezés Twitter-azonosítóval",
-            signOutOfFacebookTooltip: "Kijelentkezés a Facebookból",
-            signOutOfGooglePlusTooltip: "Kijelentkezés a Google+-ból",
-            signOutOfTwitterTooltip: "Kijelentkezés a Twitterből"
-        },
-        dynamic_form: {  // General-purpose form; used to receive comment entry
-            optionalFormItemFlag: " (választható)",
-            requiredFormItemFlag: " (kötelező)",
-            unsettableRequiredField: "Egy kötelező mező nincs inicializálva vagy nincs az űrlapon",
-            countOfRemainingCharactersTooltip: "Fennmaradó karakterek",
-            attachmentsHeading: "Csatolmányok",
-            addAttachmentTooltip: "Csatolmány hozzáadása",
-            removeAttachmentTooltip: "Csatolmány eltávolítása",
-            cancelButtonLabel: "Mégse",
-            submitButtonLabel: "Beküldés"
-        },
-        item_details: {  // Detailed information about an item and a list of its comments
-            likeButtonTooltip: "Szavazás az elemre",
-            likeButtonInverseTooltip: "Szavazat beszámítva",
-            commentButtonTooltip: "Hozzászólás hozzáadása",
-            gotoMapViewTooltip: "Váltás térképnézetre",
-            galleryButtonTooltip: "Csatolt fájlok megtekintése",
-            commentsListHeading: "Hozzászólások",
-            noCommentsPlaceholder: "Nincs hozzászólás",
-            numberOfAttachmentsUploaded: "${0} csatolmány feltöltve",
-            numberOfAttachmentsUploadedAndFailed: "${0} csatolmány feltöltve<br>${1} csatolmányt nem sikerült feltölteni"
-        },
-        item_list: {  // List of feature layer items
-            linkToMapViewOptionLabel: "Lista szűrése térkép alapján",
-            linkToMapViewOptionTooltip: "Az aktuális térképen látható vektoros elemek felsorolása",
-            likesForThisItemTooltip: "Elem szavazatai"
-        }
-    }),
-    "ar": 1,
-    "bs": 1,
-    "ca": 0,
-    "cs": 1,
-    "da": 1,
-    "de": 1,
-    "el": 1,
-    "es": 1,
-    "et": 1,
-    "fi": 1,
-    "fr": 1,
-    "he": 1,
-    "hi": 1,
-    "hr": 1,
-    "hu": 0,
-    "id": 1,
-    "it": 1,
-    "ja": 1,
-    "ko": 1,
-    "lt": 1,
-    "lv": 1,
-    "nb": 1,
-    "nl": 1,
-    "pl": 1,
-    "pt-br": 1,
-    "pt-pt": 1,
-    "ro": 1,
-    "ru": 1,
-    "sl": 1,
-    "sr": 1,
-    "sv": 1,
-    "th": 1,
-    "tr": 1,
-    "vi": 1,
-    "zh-cn": 1,
-    "zh-hk": 1,
-    "zh-tw": 1
+  "map": {
+    "error": "Nem sikerült létrehozni a térképet",
+    "layerLoad": "Nem sikerült teljesen betölteni a térképet",
+    "missingItemsFeatureLayer": "Konfigurálja az alkalmazást a vektoros réteg használatára"
+  },
+  "sidebar_header": {
+    "menuButtonTooltip": "Menü megjelenítése",
+    "signInButton": "Bejelentkezés",
+    "signInButtonTooltip": "Bejelentkezés",
+    "signOutButton": "Kijelentkezés",
+    "signOutButtonTooltip": "Kijelentkezés",
+    "helpButtonLabel": "Súgó",
+    "helpButtonTooltip": "További információk",
+    "gotoListViewLabel": "Listanézet",
+    "gotoListViewTooltip": "Váltás listanézetre",
+    "gotoMapViewLabel": "Térképnézet",
+    "gotoMapViewTooltip": "Váltás térképnézetre"
+  },
+  "popup_Close": {
+    "closeButtonTooltip": "Bezárás"
+  },
+  "social_media": {
+    "signInHeaderTitle": "Bejelentkezés közösségi médián keresztül",
+    "signIntoFacebookTooltip": "Bejelentkezés Facebook-azonosítóval",
+    "signIntoGooglePlusTooltip": "Bejelentkezés Google+-azonosítóval",
+    "signIntoTwitterTooltip": "Bejelentkezés Twitter-azonosítóval",
+    "signOutOfFacebookTooltip": "Kijelentkezés a Facebookból",
+    "signOutOfGooglePlusTooltip": "Kijelentkezés a Google+-ból",
+    "signOutOfTwitterTooltip": "Kijelentkezés a Twitterből"
+  },
+  "dynamic_form": {
+    "optionalFormItemFlag": " (választható)",
+    "requiredFormItemFlag": " (kötelező)",
+    "unsettableRequiredField": "Egy kötelező mező nincs inicializálva vagy nincs az űrlapon",
+    "countOfRemainingCharactersTooltip": "Fennmaradó karakterek",
+    "attachmentsHeading": "Csatolmányok",
+    "addAttachmentTooltip": "Csatolmány hozzáadása",
+    "removeAttachmentTooltip": "Csatolmány eltávolítása",
+    "cancelButtonLabel": "Mégse",
+    "submitButtonLabel": "Beküldés"
+  },
+  "item_details": {
+    "likeButtonTooltip": "Szavazás az elemre",
+    "likeButtonInverseTooltip": "Szavazat beszámítva",
+    "commentButtonTooltip": "Hozzászólás hozzáadása",
+    "gotoMapViewTooltip": "Váltás térképnézetre",
+    "galleryButtonTooltip": "Csatolt fájlok megtekintése",
+    "commentsListHeading": "Hozzászólások",
+    "noCommentsPlaceholder": "Nincs hozzászólás",
+    "numberOfAttachmentsUploaded": "${0} csatolmány feltöltve",
+    "numberOfAttachmentsUploadedAndFailed": "${0} csatolmány feltöltve<br>${1} csatolmányt nem sikerült feltölteni"
+  },
+  "item_list": {
+    "linkToMapViewOptionLabel": "Lista szűrése térkép alapján",
+    "linkToMapViewOptionTooltip": "Az aktuális térképen látható vektoros elemek felsorolása",
+    "likesForThisItemTooltip": "Elem szavazatai"
+  }
 });

f52349a36d39127b183859839894c24482e6e4d6
Mike Tschudi <mtschudi@esri.com>
2020-11-23 10:16:22 -0800

    Add Slovak

---
+++ js/nls/sk/resources.js
@@ -0,0 +1,92 @@
+﻿/*
+ | Copyright 2014 Esri
+ |
+ | Licensed under the Apache License, Version 2.0 (the "License");
+ | you may not use this file except in compliance with the License.
+ | You may obtain a copy of the License at
+ |
+ |    http://www.apache.org/licenses/LICENSE-2.0
+ |
+ | Unless required by applicable law or agreed to in writing, software
+ | distributed under the License is distributed on an "AS IS" BASIS,
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ | See the License for the specific language governing permissions and
+ | limitations under the License.
+ */
+define({
+  "map": {
+    "error": "Impossible de créer la carte",
+    "licenseError": {
+      "message": "La licence de votre compte ne permet pas d’utiliser des applications configurables non publiques. Demandez à l’administrateur de votre organisation de vous attribuer un type d’utilisateur qui inclut une licence Essential Apps ou une licence Essential Apps additionnelle.",
+      "title": "Pas de licence"
+    },
+    "layerLoad": "Impossible de charger entièrement la carte",
+    "missingItemsFeatureLayer": "Veuillez configurer l'application pour utiliser une couche d'entités"
+  },
+  "sidebar_header": {
+    "menuButtonTooltip": "Afficher le menu",
+    "signInButton": "Se connecter",
+    "signInButtonTooltip": "Connexion",
+    "signOutButton": "Se déconnecter",
+    "signOutButtonTooltip": "Déconnexion",
+    "helpButtonLabel": "Aide",
+    "helpButtonTooltip": "En savoir plus",
+    "filterButtonLabel": "Filtre",
+    "gotoListViewLabel": "Vue Liste",
+    "gotoListViewTooltip": "Accéder à la vue Liste",
+    "gotoMapViewLabel": "Vue cartographique",
+    "gotoMapViewTooltip": "Accéder à la vue cartographique"
+  },
+  "popup_Close": {
+    "closeButtonTooltip": "Fermer"
+  },
+  "social_media": {
+    "signInHeaderTitle": "Connexion via un réseau social",
+    "signIntoFacebookTooltip": "Se connecter avec Facebook",
+    "signIntoGooglePlusTooltip": "Se connecter avec Google+",
+    "signIntoTwitterTooltip": "Se connecter avec Twitter",
+    "signOutOfFacebookTooltip": "Se déconnecter de Facebook",
+    "signOutOfGooglePlusTooltip": "Se déconnecter de Google+",
+    "signOutOfTwitterTooltip": "Se déconnecter de Twitter"
+  },
+  "dynamic_form": {
+    "optionalFormItemFlag": " (facultatif)",
+    "requiredFormItemFlag": " (requis)",
+    "unsettableRequiredField": "Un champ obligatoire n'est pas initialisé ou ne figure pas dans le formulaire",
+    "countOfRemainingCharactersTooltip": "Caractères restants",
+    "attachmentsHeading": "Pièces jointes",
+    "addAttachmentTooltip": "Ajouter une pièce jointe",
+    "removeAttachmentTooltip": "Supprimer une pièce jointe",
+    "cancelButtonLabel": "Annuler",
+    "submitButtonLabel": "Envoyer"
+  },
+  "item_details": {
+    "likeButtonTooltip": "Voter pour cet élément",
+    "likeButtonInverseTooltip": "Vote comptabilisé",
+    "commentButtonTooltip": "Ajouter un commentaire",
+    "gotoMapViewTooltip": "Accéder à la vue cartographique",
+    "galleryButtonTooltip": "Voir les fichiers joints",
+    "commentsListHeading": "Commentaires",
+    "noCommentsPlaceholder": "Aucun commentaire",
+    "numberOfAttachmentsUploaded": "${0} pièces(s) jointe(s) chargée(s)",
+    "numberOfAttachmentsUploadedAndFailed": "${0} pièces(s) jointe(s) chargée(s)<br>${1} pièces jointes n’ont pas pu être chargées"
+  },
+  "item_list": {
+    "linkToMapViewOptionLabel": "Filtrer la liste par cartes",
+    "linkToMapViewOptionTooltip": "Répertorier les entités visibles sur la carte active",
+    "likesForThisItemTooltip": "Voter pour cet élément"
+  },
+  "filter": {
+    "filterPanelHeaderText": "Filtres définis",
+    "selectOption": "Sélectionner",
+    "applyButton": "Appliquer",
+    "hintLabel": "Astuce : ${hintLabelText}",
+    "uniqueRadioBtnLabel": "unique",
+    "valueRadioBtnLabel": "valeur",
+    "andTextLabel": "et",
+    "noFilterConfiguredMessage": "Aucun filtre 'Demander des valeurs ou activer le temps sur une couche' n’est configuré dans la carte Web",
+    "singleFieldDateTimeLabel": "Date et heure de l’événement",
+    "multiFieldStartDateTimeLabel": "Date et heure de début",
+    "multiFieldEndDateTimeLabel": "Date et heure de fin"
+  }
+});

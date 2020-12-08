7621c4a78e60c30220a14ae09e680deb0b8e0ad8
Ryan Cosby <rcosby@esri.com>
2020-12-08 08:20:13 -0800

    remove change to template

--- js/template.js
+++ js/template.js
@@ -144,7 +144,6 @@ define([
                                 deferred.reject(new Error(licenseMessage));
                             }
                         }
-                        this.config.templateConfig = this.templateConfig;
                         // We have all we need, let's set up a few things
                         this._completeApplication();
                         deferred.resolve(this.config);
